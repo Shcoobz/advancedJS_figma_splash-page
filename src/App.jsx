e758ba228469cdc4d9683935966e4649b046df93
Shcoobz <a.boeckmann23@gmail.com>
2024-04-24 14:44:24 +0200

    implemented BrandImgae

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import BrandImage from './components/BrandImage';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className='wrapper'>
+      <BrandImage />
     </div>
   );
 }
---
+++ src/components/BrandImage.jsx
@@ -0,0 +1,7 @@
+import brandLogo from '/img/habitual-branding.png';
+
+function BrandImage() {
+  return <img src={brandLogo} alt='Habitual Logo' className='brand-img' />;
+}
+
+export default BrandImage;
